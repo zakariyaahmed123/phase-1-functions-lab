0e052c5350590b75309bc5251bc6ab465435d182
zakariyaahmed123 <118251771+zakariyaahmed123@users.noreply.github.com>
2022-12-08 09:51:42 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solution in this file!
+/ Code your solution in this file!
+function distanceFromHqInBlocks(numberOfBlocks) {
+if (numberOfBlocks < 42) {
+    return 42 - numberOfBlocks;
+} else {
+    return numberOfBlocks - 42;
+}
+}
+function distanceFromHqInFeet(numberOfBlocks) {
+    return distanceFromHqInBlocks(numberOfBlocks) * 264;
+}
+function distanceTravelledInFeet(start, destination) {
+    if (destination < start) {
+        return (start - destination) * 264;
+    } else {
+        return (destination - start) * 264;
+    }
+}
+
